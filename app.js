560bda84b0a6d1bdfb6bada15cb99dc00f95bda7
EmilyDSarani <emily.sarani@gmail.com>
2021-08-17 17:13:18 -0500

    I forgot to commit a bit ago! oops

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 // import functions and grab DOM elements
-const userGuessEl=document.getElementById('press-guess');
-const userResetEl=document.getElementById('press-reset');
+import { comparison } from './utils.js';
 
+const userGuessEl = document.getElementById('number-input');
+const guessButtonEl = document.getElementById('guess-button');
+const resultsEl = document.getElementById('results');
 
+let correctNumber = Math.ceil(Math.random() * 20);
 
+let remains = 3;
 
+guessButtonEl.addEventListener('click', () => {
+
+    let currentGuess = (Number(userGuessEl.value));
+    let res = comparison (currentGuess, correctNumber);
+
+    if (res === 0){
+        resultsEl.textContent = `Ding Ding Ding! Correct!`;
+    } else if (res === 1){
+        resultsEl.textContent = `Big Oof, that\'s too high my dude, you have ${remains}`;
+    } else {
+        resultsEl.textContent = `Woooooah there, thatta there is done too low${remains}`;
+    }
+
+});
 
 
 // initialize global state
---
+++ utils.js
@@ -0,0 +1,10 @@
+export function comparison(expected, correctNumber)
+
+{if (expected === correctNumber) {
+    return 0;
+} else if (expected > correctNumber) {
+    return 1;
+} else {
+    return -1; 
+}
+}
