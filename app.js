eac8c2fdc1d57deda3855e049a00923525d6bbe1
EmilyDSarani <emily.sarani@gmail.com>
2021-08-17 17:56:03 -0500

    FIGURED OUT HOW TO DO THE GUESS COUNT. I was about to give up and ask for help, but i did it!!

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ import { comparison } from './utils.js';
 const userGuessEl = document.getElementById('number-input');
 const guessButtonEl = document.getElementById('guess-button');
 const resultsEl = document.getElementById('results');
+const guessesEl = document.getElementById ('guesses-remaining');
 
 let correctNumber = Math.ceil(Math.random() * 20);
 
-let remains = 3;
+let numberofGuesses = 3;
+let remains = numberofGuesses;
 
 guessButtonEl.addEventListener('click', () => {
 
@@ -17,9 +19,9 @@ guessButtonEl.addEventListener('click', () => {
     if (res === 0){
         resultsEl.textContent = `Ding Ding Ding! Correct!`;
     } else if (res === 1){
-        resultsEl.textContent = `Big Oof, that\'s too high my dude, you have ${remains}`;
+        resultsEl.textContent = `Big Oof, that\'s too high my dude, you have ${remains--} guesses left`;
     } else {
-        resultsEl.textContent = `Woooooah there, thatta there is done too low${remains}`;
+        resultsEl.textContent = `Woooooah there, thatta there is done too low ${remains--} guesses left`;
     }
 
 });
